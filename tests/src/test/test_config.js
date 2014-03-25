1f4fcefaf767217d8b9806ec672f11ab817bc211
danielgraham <dpgraham.85@gmail.com>
2014-03-24 19:17:05 -0700

    More grunting

--- tests/src/test/test_config.js
+++ tests/src/test/test_config.js
@@ -1,4 +1,4 @@
 testConfig = {
-    "domainOne": false ? "http://localhost:9001/" : "http://localhost:9001/",
-    "domainTwo": false ? "http://localhost:9002/" : "http://localhost:9001/"
+    "domainOne": true ? "http://localhost:9001/" : "http://localhost:9001/",
+    "domainTwo": true ? "http://localhost:9002/" : "http://localhost:9001/"
 }
